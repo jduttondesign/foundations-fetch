4f1fd077571f4fade77e8a7b4586ccb9ce07f395
Jennifer Dutton <jenniferdutton@Jennifers-MacBook-Pro.local>
2017-01-10 19:27:26 -0600

    initial

---
+++ app.js
@@ -0,0 +1,12 @@
+;(function () {
+	window.app = { myApp: "yes"};
+})();//wrap in an iffy 
+
+//
+// ;(function() {
+// 	var myFunc = () => console.log('function 1')
+// })();
+
+// ;(function() {
+// 	var myFunc = () => console.log('function 2')
+// })();
