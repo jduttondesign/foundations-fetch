5de6573ad4faa384d151f9a70cb38061d81f74a7
Jennifer Dutton <jenniferdutton@Jennifers-MacBook-Pro.local>
2017-01-12 14:44:52 -0600

    working thru functions on mainCtrl

--- mainController.js
+++ mainController.js
@@ -8,9 +8,21 @@
         const response = new Response(success);
         $scope.responses.push(response);
       }, (error) => {
-        
-        
       });
+      
+//var app = angular.module('fetchApp', []);
+// app.controller('MainController', function($scope){
+//   $scope.fetch = ["GET", "POST", "OPTIONS", "HEAD"]
+
+// $.ajax({
+//         type: 'GET',
+//         url: dataURL,
+//         dataType: 'jsonp',
+//         success: function (data) {
+//             //insert data to page
+//         }
+
+});
     };
   });
 })();
